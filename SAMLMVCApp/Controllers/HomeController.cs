68ddc160f5c1b0b993b7f45e8b896f6ac5ba1126
Ken <ken@stcyrfamily.net>
2016-07-11 00:44:47 -0400

    Initial Commit

--- SAMLMVCApp/Controllers/HomeController.cs
+++ SAMLMVCApp/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace SAMLMVCApp.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
